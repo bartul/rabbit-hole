b65ccdf2c84c870d612711fd61f9bde1c6e0eda6
Bartul Bonacic <bartul.bonacic@live.com>
2015-12-06 10:36:30 +0400

    hash calc

---
+++ program.fsx
@@ -0,0 +1,20 @@
+open System
+open System.Text
+
+let calculateHash (input:string) =
+    let md5 = System.Security.Cryptography.MD5.Create() 
+    let hash =  System.Text.Encoding.ASCII.GetBytes(input) |> md5.ComputeHash
+    let sb = System.Text.StringBuilder()
+
+    hash |> Array.iter (fun i -> (i.ToString("X2") |> sb.Append |> ignore)) 
+
+    sb.ToString()
+
+let main () =
+    let anagram = "poultry outwits ants" 
+    let md5hash = "4624d200580677270a54ccff86b9610e" 
+
+    let expectedHash = calculateHash anagram
+
+    printfn "Hello hash : '%s'." expectedHash
+
