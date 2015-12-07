e9ed6867cd7ac561c12473faa5bbfffd60a5db0a
Bartul Bonacic <bartul.bonacic@live.com>
2015-12-07 13:08:55 +0400

    little optimizations, little

--- program.fsx
+++ program.fsx
@@ -23,11 +23,9 @@ let loadWordsFromFile filename =
     |> System.IO.File.ReadAllLines
     //|> Array.take 5000
     |> Array.filter (fun i -> i.Length > 2)
+    |> Array.sortByDescending (fun i -> i.Length)
     |> Array.map stringItem
-    |> Array.sortByDescending (fun i ->  
-        match i with
-        | _, l -> l
-        )        
+
 let loadWords () = loadWordsFromFile "wordlist.txt"
 
 let printWord (word:string*int) =
@@ -39,17 +37,27 @@ let printWords (words : (string*int)[]) =
     |> Array.iter (fun i -> printWord i) 
  
  
-let rec uberCandidatesRec (input : string) (lengthLeft : int) (cursor : int) (runningSet : string[]) (candidates : System.Collections.Generic.List<string[]>) (orderedPool : (string * int)[]) =
+let rec uberCandidatesRec (input : string) (lengthLeft : int) (runningSet : string[]) (candidates : System.Collections.Generic.List<string[]>) (orderedPool : (string * int)[]) =
 
-    for i in cursor .. orderedPool.Length - 1 do
-        let item = orderedPool.[i]
+    orderedPool |> Array.Parallel.iteri (fun i item ->
+        //let item = orderedPool.[i]
         let itemValue, itemLength = item
         let itemLengthLeft = lengthLeft - itemLength
         let set = Array.append runningSet [|itemValue|]
         if itemLengthLeft > 0 && i < orderedPool.Length - 1 then 
-            uberCandidatesRec input itemLengthLeft (i + 1) set candidates orderedPool 
+            uberCandidatesRec input itemLengthLeft set candidates (Array.sub orderedPool (i + 1) (orderedPool.Length - i - 1)) 
         elif itemLengthLeft = 0 && alphabetize(String.Concat(set)) = alphabetize(input) then
             candidates.Add set
+    )
+    // for i in 0 .. orderedPool.Length - 1 do
+    //     let item = orderedPool.[i]
+    //     let itemValue, itemLength = item
+    //     let itemLengthLeft = lengthLeft - itemLength
+    //     let set = Array.append runningSet [|itemValue|]
+    //     if itemLengthLeft > 0 && i < orderedPool.Length - 1 then 
+    //         uberCandidatesRec2 input itemLengthLeft set candidates (Array.sub orderedPool (i + 1) (orderedPool.Length - i - 1)) 
+    //     elif itemLengthLeft = 0 && alphabetize(String.Concat(set)) = alphabetize(input) then
+    //         candidates.Add set
 
 let printCandidates (candidates : System.Collections.Generic.IEnumerable<string[]>) =
     for item in candidates do
@@ -61,11 +69,11 @@ let main () =
     let md5hash = "4624d200580677270a54ccff86b9610e" 
 
     let candidates = System.Collections.Generic.List<string[]>()
-    let value = "stopsita"
+    let value = "stopitsa   "
     let length = value.Length
 
     let words = loadWords() // candidatesFromFile("ant")
-    words |> uberCandidatesRec value length 0 [||] candidates 
+    words |> uberCandidatesRec value length [||] candidates 
     candidates |> printCandidates
     
 //    words |> printWords  
