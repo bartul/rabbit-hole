cba502e03e9d04e871ee1d1f1bc51eb22ac33a28
Bartul Bonacic <bartul.bonacic@live.com>
2015-12-10 11:55:30 +0400

    Letter based huge optimization

--- program.fsx
+++ program.fsx
@@ -11,22 +11,30 @@ let calculateHash (input:string) =
 
     sb.ToString()
 
+
 let alphabetize (value:string) = 
     String.Concat(value.ToCharArray() 
     |> Array.sort  
     |> Array.map (fun i -> string i))
 let stringItem (value:string) = value, value.Length
 let areAlphaEquale value1 value2 = alphabetize value1 = alphabetize value2
-    
-let loadWordsFromFile filename = 
+
+let hasUnusedChars (focus : string) (valueToCheck : string)=
+    valueToCheck.ToCharArray() |> Array.exists (fun item ->  
+        //printfn "item: %c, focus.IndexOf(item): %d" item (focus.IndexOf(item))
+        focus.IndexOf(item) = -1
+    )
+
+let loadWordsFromFile filename input = 
     System.IO.Path.Combine(System.Environment.CurrentDirectory, filename) 
     |> System.IO.File.ReadAllLines
     //|> Array.take 5000
-    // |> Array.filter (fun i -> i.Length > 1)
-    |> Array.sortBy (fun i -> i.Length)
+    |> Array.filter (fun i -> i.Length > 1)
+    |> Array.filter ((fun i -> hasUnusedChars input i) >> not)
+    |> Array.sortByDescending (fun i -> i.Length)
     |> Array.map stringItem
 
-let loadWords () = loadWordsFromFile "wordlist.txt"
+let loadWords input = loadWordsFromFile "wordlist.txt" input
 
 let printWord (word:string*int) =
     match word with
@@ -55,26 +63,33 @@ let rec uberCandidatesRec (input : string) (lengthLeft : int) (runningSet : stri
             uberCandidatesRec input itemLengthLeft set candidates (Array.sub orderedPool (i + 1) (orderedPool.Length - i - 1)) 
         elif itemLengthLeft = 0 && alphabetize(String.Concat(set)) = alphabetize(input) then
             candidates.Add set
-            printCandidate set
+            printCandidate set  
     )
 let ThreeSumCandidates (orderedPool : (string * int)[]) (input : string) =
-    let length = input.Length
-    for i in 0 .. orderedPool.Length - 3 do
-        let aValue, aLength = orderedPool.[i]
+    if input.Length % 3 <> 0 then printfn "There will be errors." 
+    let substract = input.Length / 3
+    let list = orderedPool |> Array.map ( fun item ->
+        let itemV, itemC = item
+        (itemV, itemC - substract))
+
+    for i in 0 .. list.Length - 3 do
+        let aValue, aLength = list.[i]
         let mutable start = i + 1
-        let mutable finnish = orderedPool.Length - 1
+        let mutable finnish = list.Length - 1
         while start < finnish do    
-            let bValue, bLength = orderedPool.[start]  
-            let cValue, cLength = orderedPool.[finnish]
+            let bValue, bLength = list.[start]  
+            let cValue, cLength = list.[finnish]
             let set = [|aValue; bValue; cValue|]
-            if (aLength + bLength + cLength = length) && alphabetize(String.Concat(set)) = alphabetize(input) then
-                printCandidate set
-                let start = start + 1
-                finnish = finnish - 1
-            elif (aLength + bLength + cLength > length) then
-                finnish = finnish - 1
+            printf "i: %d, start: %d, finnish: %d, a+b+c: %d," i start finnish (aLength + bLength + cLength)
+            printCandidate set
+            if (aLength + bLength + cLength = 0) then
+                if alphabetize(String.Concat(set)) = alphabetize(input) then printfn "BINGO!" //printCandidate set
+                start <- start + 1
+                finnish <- finnish - 1
+            elif (aLength + bLength + cLength > 0) then
+                finnish <- finnish - 1
             else 
-                start = start + 1
+                start <- start + 1
     
     //     let itemLengthLeft = lengthLeft - itemLength
     //     let set = Array.append runningSet [|itemValue|]
@@ -89,23 +104,25 @@ let main () =
     let md5hash = "4624d200580677270a54ccff86b9610e" 
 
     let candidates = System.Collections.Generic.List<string[]>()
-    let value = "poultryoutwitsants"
+    let value = "poultryoutwitsants" //"poultryoutwitsants"
     let length = value.Length
 
-    let words = loadWords() // candidatesFromFile("ant")
-//    words |> uberCandidatesRec value length [||] candidates 
+    let words = loadWords value  // candidatesFromFile("ant")
 
     printfn "--------- List Candidates --------------"
-    ThreeSumCandidates words "ana"
-    //candidates |> printCandidates
+//    words |> uberCandidatesRec value length [||] candidates 
+//    candidates |> printCandidates
+
+    ThreeSumCandidates words value
     
-//    words |> printWords  
+    //words |> printWords  
+    words |> Array.length |> printfn "Dictionary Count: %d"  
 
     ""
 
     //words |> printWords
     // let expectedHash = calculateHash anagram
-//main()
+main()
 
         
 
